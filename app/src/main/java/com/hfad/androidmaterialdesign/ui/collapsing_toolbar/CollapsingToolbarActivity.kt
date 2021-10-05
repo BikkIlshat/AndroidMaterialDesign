4927eda2dcca6f19a1c559f2a5af87dfc9ed3141
BikkIlshat <HeavyIlshat@rambler.ru>
2021-10-05 22:26:13 +0300

    Создал класс CollapsingToolbarActivity.kt создал layout collapsing_toolbar_activity.xml, создал  кастомные темы для COLLAPSING_TOOLBAR_LAYOUT

---
+++ app/src/main/java/com/hfad/androidmaterialdesign/ui/collapsing_toolbar/CollapsingToolbarActivity.kt
@@ -0,0 +1,11 @@
+package com.hfad.androidmaterialdesign.ui.collapsing_toolbar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CollapsingToolbarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
